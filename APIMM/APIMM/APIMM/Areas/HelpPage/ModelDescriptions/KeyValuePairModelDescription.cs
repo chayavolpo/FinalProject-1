9c72c626599b03620880951658f12fa608d82ba3
Racheli314 <rachelic1999@gmail.com>
2020-07-22 00:02:48 +0300

    APIMM update addclerk and add shelter

---
+++ APIMM/APIMM/APIMM/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
@@ -0,0 +1,9 @@
+namespace APIMM.Areas.HelpPage.ModelDescriptions
+{
+    public class KeyValuePairModelDescription : ModelDescription
+    {
+        public ModelDescription KeyModelDescription { get; set; }
+
+        public ModelDescription ValueModelDescription { get; set; }
+    }
+}
---
+++ APIMM/APIMM/DTO/AmountPeopleInShelterDTO.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL;
+
+namespace DTO
+{
+     public class AmountPeopleInShelterDTO
+     {
+        static DAL.DataBaseMMEntities db = new DAL.DataBaseMMEntities();
+        public int AmountpplKey { get; set; }
+        public int ShelterKey { get; set; }
+        public Nullable<int> AmountPPLSpeDH { get; set; }
+        public Nullable<System.DateTime> Date { get; set; }
+        public Nullable<System.TimeSpan> Time { get; set; }
+
+        public AmountPeopleInShelterDTO(DAL.AmountPeopleInShelter amountpplDAL)
+        {
+            AmountpplKey = amountpplDAL.AmountpplKey;
+            ShelterKey = amountpplDAL.ShelterKey;
+            AmountPPLSpeDH = amountpplDAL.AmountPPLSpeDH;
+            Date = amountpplDAL.Date;
+            Time = amountpplDAL.Time;
+        }
+        public AmountPeopleInShelterDTO()
+        {
+          
+
+        }
+
+        public static DAL.AmountPeopleInShelter DtoToDAL(AmountPeopleInShelterDTO amountPPLDTO)
+        {
+            DAL.AmountPeopleInShelter amountPPLDAL = new DAL.AmountPeopleInShelter();
+            amountPPLDAL.AmountpplKey = amountPPLDTO.AmountpplKey;
+            amountPPLDAL.ShelterKey = amountPPLDTO.ShelterKey;
+            amountPPLDAL.AmountPPLSpeDH = amountPPLDTO.AmountPPLSpeDH;
+            amountPPLDAL.Date = amountPPLDTO.Date;
+            amountPPLDAL.Time = amountPPLDTO.Time;
+            return amountPPLDAL;
+        }
+
+        public static bool UpdateAmountPPLInShelter(AmountPeopleInShelterDTO amountPPLDTO)
+        {
+            DAL.AmountPeopleInShelter amountPPLDAL = DtoToDAL(amountPPLDTO);
+            try
+            {
+                db.AmountPeopleInShelters.Add(amountPPLDAL);
+                db.SaveChanges();
+                return true;
+            }
+            catch(Exception e)
+            {
+                return false;
+            }
+        }
+
+        
+    }
+}
